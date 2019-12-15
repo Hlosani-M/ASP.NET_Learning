f4193c71cd65b7dc0028bbce8dea1d4da70e073c
Hlosani Dube <hlosani.dube@gmail.com>
2019-12-15 20:30:50 +0200

    some progress

--- OdeToFood.Data/Services/InMemoryRestaurantData.cs
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -19,9 +19,39 @@ namespace OdeToFood.Data.Services
             };
         }
 
+        public void Add(Restaurant restaurant)
+        {
+            restaurants.Add(restaurant);
+            restaurant.Id = restaurants.Max(r => r.Id) + 1;
+        }
+
+        public void Delete(int id)
+        {
+            var restaurant = Get(id);
+            if (restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+        }
+
+        public Restaurant Get(int id)
+        {
+            return restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
         public IEnumerable<Restaurant> GetAll()
         {
             return restaurants.OrderBy(r => r.Name);
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            var existingRestaurant = Get(restaurant.Id);
+            if (existingRestaurant != null)
+            {
+                existingRestaurant.Name = restaurant.Name;
+                existingRestaurant.Cuisine = restaurant.Cuisine;
+            }
         }
     }
 }
--- OdeToFood.Web/Global.asax.cs
+++ OdeToFood.Web/Global.asax.cs
@@ -18,6 +18,7 @@ namespace OdeToFood.Web
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ContainerConfig.RegisterContainer(GlobalConfiguration.Configuration);
         }
     }
 }
